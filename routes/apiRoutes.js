898a971e8ca95c76a013583c04675d1a6bad52b1
Elias Calagiu <eliasc81@gmail.com>
2021-02-16 16:55:06 -0800

    switched code from apiRoutes to htmlRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,5 @@ const router = express.Router();
 const path = require("path")
 const db = require("../db/db.json")
 
-router.get("/", (req,res) =>{
-res.sendFile(path.join(_dirname, "/public/index.html"))
-})
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,14 @@ const db = require("../db/db.json")
 
 
 
+router.get("/", (req,res) =>{
+    res.sendFile(path.join(_dirname, "/public/index.html"))
+    });
+    
+    
+    router.get("/notes")
+    
+    module.exports = router;
+    
+
 module.exports = router;
