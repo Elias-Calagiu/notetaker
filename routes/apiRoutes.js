03da68d86d7b95b65811daf4a03f4b2bf8f9c1c6
Elias Calagiu <eliasc81@gmail.com>
2021-02-16 17:02:28 -0800

    htmlRoutes functional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,4 +4,12 @@ const path = require("path")
 const db = require("../db/db.json")
 
 
+router.get("/api/notes", (req,res)=>{
+
+})
+
+router.post("/api/notes", (req,res)=>{
+    
+})
+
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,17 @@
+//Dependencies
 const express = require("express");
 const router = express.Router();
 const db = require("../db/db.json")
+const path = require("path")
 
-
-
+// Get requests
 router.get("/", (req,res) =>{
-    res.sendFile(path.join(_dirname, "/public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
     });
     
     
-    router.get("/notes")
+    router.get("/notes", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../public/notes.html"))
+    })
     
     module.exports = router;
-    
-
-module.exports = router;
