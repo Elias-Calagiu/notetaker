7f2b25e696975af2af76c1941ad17f54aaed8ecf
Elias Calagiu <eliasc81@gmail.com>
2021-02-16 18:18:36 -0800

    post route populates added note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+// const { equal } = require("assert");
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
@@ -5,19 +6,32 @@ const path = require("path")
 const db = require("../db/db.json")
 
 
-router.get("/api/notes", (req,res)=>{
-fs.readFile("db/db.json", "utf-8", (err,data)=>{
-    if(err)
-    throw(err)
-    res.json(JSON.parse(data))
-})
+router.get("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+        if (err)
+            throw (err)
+        res.json(JSON.parse(data))
+    })
 })
 
-router.post("/api/notes", (req,res)=>{
-    fs.readFile("db/db.json", "utf-8", (err,data)=>{
-        if(err)
-        throw(err)
-        res.json(JSON.parse(data))
+router.post("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
+        let note= []
+        if (err)
+            throw (err)
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: req.params.id,
+        }
+        note.push(newNote);
+        console.log(newNote);
+
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(note), err => {
+            if (err)
+                throw (err)
+            res.send(note)
+        })
     })
 })
 
