14ab9f1ed6d07ab8757be67f6de8345ced48d82c
Elias Calagiu <eliasc81@gmail.com>
2021-02-16 16:50:32 -0800

    rendering home page on local host with get route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path")
 const db = require("../db/db.json")
 
-
-
+router.get("/", (req,res) =>{
+res.sendFile(path.join(_dirname, "/public/index.html"))
+})
 
 module.exports = router;
