3f4d7091373478aeba362c7ddeb2c87e283792b2
Elias Calagiu <eliasc81@gmail.com>
2021-02-16 17:09:00 -0800

    api/notes get route functional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 const path = require("path")
 const db = require("../db/db.json")
 
 
 router.get("/api/notes", (req,res)=>{
-
+fs.readFile("db/db.json", "utf-8", (err,data)=>{
+    if(err)
+    throw(err)
+    res.json(JSON.parse(data))
+})
 })
 
 router.post("/api/notes", (req,res)=>{
-    
+    fs.readFile("db/db.json", "utf-8", (err,data)=>{
+        if(err)
+        throw(err)
+        res.json(JSON.parse(data))
+    })
 })
 
 module.exports = router;
